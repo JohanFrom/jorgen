b532dee298381213d432921b2501678b91b45370
JohanFrom <gouldsilver@gmail.com>
2022-12-08 15:50:23 +0100

    Added logger

--- Controllers/JorgenController.cs
+++ Controllers/JorgenController.cs
@@ -6,25 +6,35 @@ namespace jorgen.Controllers
     [Route("[controller]")]
     public class JorgenController : ControllerBase
     {
+        private readonly ILogger _logger;
+        public JorgenController(ILogger<JorgenController> logger)
+        {
+            _logger = logger;   
+        }
+
         [HttpGet]
         public IActionResult GetJorgenImage()
         {
+            _logger.LogInformation("Starting API CALL", DateTimeOffset.UtcNow);
             try
             {
-                string? imagepathName = AppDomain.CurrentDomain.BaseDirectory + "data\\jorgen.png" ?? null;
+                string? imagepathName = AppDomain.CurrentDomain.BaseDirectory + "Data\\jorgen.png" ?? null;
 
                 if (imagepathName == null)
                 {
+                    _logger.LogInformation("Image is null", DateTimeOffset.UtcNow);
                     return Content("Could not find the image of Jorgen");
                 }
 
                 byte[] b = System.IO.File.ReadAllBytes(imagepathName);
 
+                _logger.LogInformation("Sending image", DateTimeOffset.UtcNow);
                 return File(b, "image/png");
             }
             catch (Exception e)
             {
-                return Content($"Could not GET image. {Environment.NewLine} Error message: {e.Message} ");
+                _logger.LogInformation("Error message: {e.Message}", DateTimeOffset.UtcNow);
+                return Content($"Could not GET image.{Environment.NewLine} Error message: {e.Message} ");
             }
         }
     }
