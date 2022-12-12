57d1c60a05f49c659866709a5b37ca53904d9b8e
JohanFrom <gouldsilver@gmail.com>
2022-12-12 13:42:37 +0100

    Fixed controllers

---
+++ Controllers/JorgenController.cs
@@ -0,0 +1,120 @@
+﻿using jorgen.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace jorgen.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class JorgenController : ControllerBase
+    {
+        public IConfiguration? Configuration { get; }
+        private static readonly HttpClient client = new();
+        private readonly string _url = "https://pinger-23654.azurewebsites.net/";
+        private readonly ILogger<JorgenController> _logger;
+        //private readonly string _apiKey = "0c87245268ef262893e0da7caa3d6e37";
+
+        public JorgenController(ILogger<JorgenController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            Configuration = configuration;
+        }
+
+        
+
+        //public readonly string? _apiKey = Configuration["TokenSecrets:WeatherApiKey"];
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult GetJorgenImage()
+        {
+            try
+            {
+                string? imagepathName = AppDomain.CurrentDomain.BaseDirectory + "data\\jorgenimg.png" ?? null;
+
+                if (imagepathName == null)
+                {
+                    return Content("Could not find image");
+                }
+
+                byte[] b = System.IO.File.ReadAllBytes(imagepathName);
+
+                return File(b, "image/png");
+            }
+            catch (Exception e)
+            {
+                return Content($"Could not return image.{Environment.NewLine}Error message: {e.Message} ");
+            }
+        }
+
+        [HttpGet("ping/{sleepTime}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> ListenForPing(int sleepTime)
+        {
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(_url + "Pinger/" + sleepTime);
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    return StatusCode(StatusCodes.Status404NotFound);
+                }
+
+                return StatusCode(StatusCodes.Status200OK);
+
+            }
+            catch (Exception e)
+            {
+                return Content($"Could not listen for ping.{Environment.NewLine}Error message: {e.Message} ");
+            }
+        }
+
+        [HttpGet("getweather")]
+        public async Task<ActionResult<object>> GetWeatherData()
+        {
+            try
+            {
+                string url = string.Format("https://api.openweathermap.org/data/2.5/weather?q={0}&appid={1}", "veberod", Configuration?["WeatherApiKey"]);
+                var request = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Get,
+                    RequestUri = new Uri(url)
+                };
+
+                using (var response = await client.SendAsync(request))
+                {
+                    response.EnsureSuccessStatusCode();
+
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        return Content("Could not fetch the weather api");
+                    }
+
+
+                    var body = await response.Content.ReadAsStringAsync();
+
+                    WeatherModel.Root info = JsonConvert.DeserializeObject<WeatherModel.Root>(body);
+
+                    WeatherModel.WeatherObject weatherObject = new()
+                    {
+                        temp = double.Parse((info.main.temp - 273.15).ToString("F2")),
+                        humidity = info.main.humidity,
+                        pressure = info.main.pressure,
+                        speed = info.wind.speed,
+                        main = info.weather.Select(x => x.main).FirstOrDefault() ?? null
+                    };
+                    return Ok(weatherObject);
+
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Exception message: " + e.Message);
+            }
+        }
+    }
+}
---
+++ Models/WeatherModel.cs
@@ -0,0 +1,58 @@
+﻿namespace jorgen.Models
+{
+    class WeatherModel
+    {
+        public class Coord
+        {
+            public double lon { get; set; }
+            public double lat { get; set; }
+        }
+
+        public class Weather
+        {
+            public string main { get; set; }
+            public string description { get; set; }
+        }
+
+        public class Main
+        {
+            public double temp { get; set; }
+            public double pressure { get; set; }
+            public double humidity { get; set; }
+
+        }
+
+        public class Wind
+        {
+            public double speed { get; set; }
+        }
+
+        public class Sys
+        {
+            public long sunrise { get; set; }
+            public long sunset { get; set; }
+        }
+
+        public class Root
+        {
+            public Coord coord { get; set; }
+            public List<Weather> weather { get; set; }
+            public Main main { get; set; }
+
+            public Wind wind { get; set; }
+            public Sys sys { get; set; }
+        }
+
+
+
+        public class WeatherObject
+        {
+            public double temp { get; set; }
+            public double pressure { get; set; }
+            public double humidity { get; set; }
+            public double speed { get; set; }
+
+            public string? main { get; set; }
+        }
+    }
+}
