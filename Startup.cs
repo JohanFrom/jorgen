0da1bd972cba24bfece2adb1668de44ecf6dcb52
JohanFrom <gouldsilver@gmail.com>
2022-12-22 10:22:18 +0100

    Test

--- Startup.cs
+++ Startup.cs
@@ -73,23 +73,6 @@ namespace jorgen
                 //c.RoutePrefix = string.Empty;
             });
 
-            SecretClientOptions options = new()
-            {
-                Retry =
-                {
-                    Delay = TimeSpan.FromSeconds(2),
-                    MaxDelay = TimeSpan.FromSeconds(16),
-                    MaxRetries = 5,
-                    Mode = RetryMode.Exponential
-                }
-            };
-
-            var client = new SecretClient(new Uri("https://JorgenKeyVault.vault.azure.net/"), new DefaultAzureCredential(), options);
-
-            KeyVaultSecret secret = client.GetSecret("WeatherApiKey");
-
-            string secretValue = secret.Value;
-
             app.UseCors(x => x
                 .AllowAnyOrigin()
                 .AllowAnyMethod()
